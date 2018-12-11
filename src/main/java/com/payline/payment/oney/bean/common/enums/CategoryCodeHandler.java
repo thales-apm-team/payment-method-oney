9b7892a4e64e36003ee0f00bc6a356f87866611c
houssni.mlinde <houssni.mlinde@mythalesgroup.io>
2018-12-11 14:13:07 +0100

    mapping categorie des articles  Payline - Oney

---
+++ src/main/java/com/payline/payment/oney/bean/common/enums/CategoryCodeHandler.java
@@ -0,0 +1,150 @@
+package com.payline.payment.oney.bean.common.enums;
+
+public class CategoryCodeHandler {
+    private CategoryCodeHandler() {
+    }
+
+    public static int findCategory(int paylineProductCode) {
+
+
+//todo parser en char pour eviter le probleme de taille des charactères
+            int oneyCategoryCode = 5;
+
+            switch (paylineProductCode) {
+                case 17:
+                case 170001:
+                case 170002:
+                    oneyCategoryCode = 1;
+                    break;
+                case 11:
+                case 110001:
+                    oneyCategoryCode = 2;
+                    break;
+
+                case 10:
+                case 14:
+                case 19:
+                case 24:
+                case 24001:
+//                case 2400010001:
+//                case 2400010002:
+                case 25:
+                    oneyCategoryCode = 1;
+                    break;
+
+                case 5:
+                case 50001:
+                case 500010001:
+                case 50002:
+                case 50003:
+                case 500030001:
+                case 50004:
+                case 500040001:
+                case 500040002:
+                case 500040003:
+                case 500040004:
+                case 599990001:
+                case 599990002:
+                case 12:
+                case 120001:
+                case 1200010001:
+                case 1200010002:
+                case 1200010003:
+                case 1200010004:
+                case 120002:
+                case 120003:
+                case 1200030001:
+                case 120004:
+                case 120005:
+                case 120006:
+                case 120007:
+                case 120008:
+                case 1200080001:
+                case 16:
+                    oneyCategoryCode = 4;
+                    break;
+
+
+                case 4:
+                case 40001:
+                case 400010001:
+                case 400010002:
+                case 40002:
+                case 400020001:
+                case 400020002:
+                case 400020003:
+                case 40003:
+                case 400030001:
+                case 400030002:
+                    oneyCategoryCode = 5;
+                    break;
+
+                case 21:
+                    oneyCategoryCode = 6;
+                    break;
+
+                case 15:
+                case 18:
+                case 13:
+                    oneyCategoryCode = 7;
+                    break;
+
+                case 1:
+                case 100010001:
+                case 100010002:
+                case 100010003:
+                    oneyCategoryCode = 8;
+                    break;
+
+                case 7:
+                    oneyCategoryCode = 9;
+                    break;
+
+                case 22:
+                    oneyCategoryCode = 10;
+                    break;
+
+                case 23:
+                    oneyCategoryCode = 11;
+                    break;
+
+                case 9:
+                    oneyCategoryCode = 12;
+                    break;
+
+                case 6:
+                case 8:
+                    oneyCategoryCode = 13;
+                    break;
+
+                case 20:
+                    oneyCategoryCode = 14;
+                    break;
+
+                case 2:
+                case 20001:
+                case 200010001:
+                case 200010002:
+                case 200010003:
+                case 200010004:
+                case 200010005:
+                case 200010006:
+                case 200010007:
+                case 26:
+                    oneyCategoryCode = 15;
+                    break;
+
+                case 3:
+                    oneyCategoryCode = 16;
+                    break;
+
+                default:
+                    oneyCategoryCode = 5;
+                    break;
+            }
+
+
+            return oneyCategoryCode;
+        }
+    }
+
--- src/test/java/com/payline/payment/oney/utils/PluginUtilsTest.java
+++ src/test/java/com/payline/payment/oney/utils/PluginUtilsTest.java
@@ -4,9 +4,13 @@ import com.payline.payment.oney.bean.common.purchase.Item;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Locale;
 import java.util.Map;
 
-import static com.payline.payment.oney.utils.PluginUtils.truncateLongText;
+import static com.payline.payment.oney.bean.common.enums.CategoryCodeHandler.findCategory;
+import static com.payline.payment.oney.utils.PluginUtils.*;
 
 public class PluginUtilsTest {
 
@@ -15,19 +19,19 @@ public class PluginUtilsTest {
     public void testTruncateText() {
         String longText = "Update: yes it doesn't take the slim characters into account but I don't agree with that considering everyone has different screens and fonts setup and a large portion of the people that land here on this page are probably looking for a maintained library like the above.";
         String longText2 = "Update:";
-        Map textCutted = truncateLongText( longText,  longText2, 19) ;
-        Assert.assertTrue(textCutted.get("line1").toString().length()<20);
-        Assert.assertTrue(textCutted.get("line2")==null||textCutted.get("line2").toString().length()<20);
-        Assert.assertTrue(textCutted.get("line3")==null||textCutted.get("line3").toString().length()<20);
-        Assert.assertTrue(textCutted.get("line4")==null ||textCutted.get("line4").toString().length()<20);
-        Assert.assertTrue(textCutted.get("line5")==null ||textCutted.get("line5").toString().length()<20);
+        Map textCutted = truncateLongText(longText, longText2, 19);
+        Assert.assertTrue(textCutted.get("line1").toString().length() < 20);
+        Assert.assertTrue(textCutted.get("line2") == null || textCutted.get("line2").toString().length() < 20);
+        Assert.assertTrue(textCutted.get("line3") == null || textCutted.get("line3").toString().length() < 20);
+        Assert.assertTrue(textCutted.get("line4") == null || textCutted.get("line4").toString().length() < 20);
+        Assert.assertTrue(textCutted.get("line5") == null || textCutted.get("line5").toString().length() < 20);
 
     }
 
     @Test
-    public void itemComparator(){
+    public void itemComparator() {
         ItemComparator comp = new ItemComparator();
-        Item  item1 = Item.Builder.aItemBuilder()
+        Item item1 = Item.Builder.aItemBuilder()
                 .withMainItem(0)
                 .withCategoryCode(0)
                 .withLabel("label")
@@ -36,7 +40,7 @@ public class PluginUtilsTest {
                 .withQuantity(5)
                 .withItemExternalCode("externalCode")
                 .build();
-        Item  item2 = Item.Builder.aItemBuilder()
+        Item item2 = Item.Builder.aItemBuilder()
                 .withMainItem(0)
                 .withCategoryCode(0)
                 .withLabel("label")
@@ -46,7 +50,44 @@ public class PluginUtilsTest {
                 .withItemExternalCode("externalCode")
                 .build();
 
-        int highest = comp.compare(item1,item2);
-        Assert.assertEquals(1,highest);
+        int highest = comp.compare(item1, item2);
+        Assert.assertEquals(1, highest);
+    }
+
+    @Test
+    public void testGetIsoAlpha3CodeFromCountryCode2() {
+        String country = "FR";
+        String pays = getIsoAlpha3CodeFromCountryCode2(country);
+        Assert.assertEquals("FRA",pays);
+
+    }
+
+    @Test
+    public void testGetCountryNameCodeFromCountryCode2() {
+        String country = "FR";
+        String pays = getCountryNameCodeFromCountryCode2(country);
+        Assert.assertEquals("France",pays);
+
+
+
+    }
+    @Test
+    public void testCategoryHandler(){
+        int catCodePayline = 17;
+        int catCodeOney = findCategory(catCodePayline);
+        Assert.assertEquals(1, catCodeOney);
+
+
+    }
+
+    @Test
+    public void testParseInteger(){
+        int catCodePayline = 17;
+        int catCodePayline2 = 964;
+        int catCodeOney = findCategory(catCodePayline);
+        int catCodeOney2 = findCategory(catCodePayline2);
+        Assert.assertEquals(1, catCodeOney);
+        Assert.assertEquals(5, catCodeOney2);
+
     }
 }
