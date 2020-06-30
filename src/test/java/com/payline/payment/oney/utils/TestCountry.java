3d51623e6c4f470247de2c31bc8d15c6b496e531
JanFLORENT <41673610+JanFLORENT@users.noreply.github.com>
2020-06-30 17:02:04 +0200

    Ajout de code Pays (#41)
    Co-authored-by: Martin <martin.grescu@gmail.com>

--- src/test/java/com/payline/payment/oney/utils/TestCountry.java
+++ src/test/java/com/payline/payment/oney/utils/TestCountry.java
@@ -2,7 +2,7 @@ package com.payline.payment.oney.utils;
 
 public enum TestCountry {
 
-    BE("+32"), IT("+39"), FR("+33"), ES("+34"), PT("+351");
+    BE("+32"), IT("+39"), FR("+33"), ES("+34"),SP("+34"), PT("+351");
 
     private String prefix;
 
